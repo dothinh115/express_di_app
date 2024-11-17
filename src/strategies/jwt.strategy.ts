6a0952b666aefb4b9126edc7675f5b5656272ea1
dothinh115 <111167728+dothinh115@users.noreply.github.com>
2024-11-17 15:29:19 +0700

    update

--- src/strategies/jwt.strategy.ts
+++ src/strategies/jwt.strategy.ts
@@ -1,7 +1,6 @@
 import { ExtractJwt, Strategy, VerifiedCallback } from "passport-jwt";
 import { Injectable } from "../core/decorators/injectable.decorator";
 import { JwtPayload } from "jsonwebtoken";
-import { Inject } from "../core/decorators/param.decorator";
 import { PassportService } from "../services/passport.service";
 import { Request } from "../core/utils/types";
 
